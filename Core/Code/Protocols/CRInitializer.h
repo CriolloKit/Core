8f232f9c03cca01062b67f36fb49ccbde865498d
TheSooth <thesooth@aol.com>
2013-09-20 22:50:14 +0300

    adding initializer

---
+++ Core/Code/Protocols/CRInitializer.h
@@ -0,0 +1,15 @@
+//
+//  CRInitializer.h
+//  Core
+//
+//  Created by TheSooth on 9/20/13.
+//  Copyright (c) 2013 CriolloKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CRInitializer <NSObject>
+
+- (void)initialize;
+
+@end
